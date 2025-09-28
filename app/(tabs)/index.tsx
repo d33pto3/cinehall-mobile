2466cedf59298ec20eaa8aeccba551fdfe23b723
d33pto3 <rakinfrhn@gmail.com>
2025-09-29 00:24:23 +0600

    ui: add tab icons

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,9 @@
-import { Link, router } from "expo-router";
-import { Pressable, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
 const HomePage = () => {
   return (
     <View>
       <Text>Home Page</Text>
-      <Link href="/tab_1">Go to tab 1</Link>
-      <Pressable onPress={() => router.push("/tab_2")}>
-        <Text>Go to tab 2</Text>
-      </Pressable>
     </View>
   );
 };
